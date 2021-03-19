d8af79dc8d20449a8be670d691746cba74142ee3
ellieleep <32715340+ellieleep@users.noreply.github.com>
2021-03-18 20:17:02 -0400

    added files

---
+++ Chapter 1/abcTraceback.py
@@ -0,0 +1,16 @@
+def a():
+    print('Start of a()')
+    b()  # call b().
+
+
+def b():
+    print('Start of b()')
+    c()  # call c().
+
+
+def c():
+    print('Start of c()')
+    42 / 0  # this will cause a zero divide error.
+
+
+a()
